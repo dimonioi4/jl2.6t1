dcd68205991570b89b51230563c53decf56b6509
dimonioi4 <dimonioi4@yandex.ru>
2021-03-01 00:51:50 +0300

    added radio

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -5,16 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class RadioTest {
     Radio radio = new Radio();
 
-    @Test
-    public void validateChangeNowPlay() {
-        radio.setNowPlay(10);
-        assertEquals(0, radio.getNowPlay());
-    }
+
 
 
     @Test
     public void validateChangeNowVolume() {
         radio.setNowVolume(11);
-        assertEquals(10, radio.getNowVolume());
+        assertEquals(0, radio.getNowVolume());
+    }
+
+    @Test
+    public void validateChangeNowPlay() {
+        radio.setNowPlay(11);
+        assertEquals(0, radio.getNowPlay());
     }
 }
