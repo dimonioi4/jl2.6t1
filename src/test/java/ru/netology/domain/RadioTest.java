b1ca4ff148ebba0fb69cf17354a578b23905b05d
dimonioi4 <dimonioi4@yandex.ru>
2021-04-01 16:01:51 +0300

    update radiotest

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -7,7 +7,6 @@ class RadioTest {
 
 
 
-
     @Test
     public void validateChangeNowVolume() {
         radio.setNowVolume(11);
@@ -19,6 +18,52 @@ class RadioTest {
     public void validateChangeNowPlay() {
         radio.setNowPlay(11);
         assertEquals(0, radio.getNowPlay());
+    }
+
+
+    @Test
+    public void shouldRadioStationMax() {
+        assertEquals(9, radio.getMaxStation());
+    }
+
+
+
+    @Test
+    public void shouldRadioStationOverMax() {
+        radio.setMaxStation(10);
+        assertEquals(10, radio.getMaxStation());
+    }
+
+
+    @Test
+    public void shouldRadioStationOverMin() {
+        radio.setNowPlay(-3);
+        assertEquals(0, radio.getMinStation());
+    }
+
+
+
+
+    @Test
+    public void shouldVolumeDown() {
+        radio.getNowVolume();
+        assertEquals(0, radio.getNowVolume());
+    }
+
+
+
+
+    @Test
+    public void shouldSetNowVolume() {
+        radio.setNowVolume(5);
+        assertEquals(5, radio.getNowVolume());
+    }
+
+
+    @Test
+    public void shouldSetNowVolumeNegative() {
+        radio.setNowVolume(-1);
+        assertEquals(0, radio.getNowVolume());
     }
 
 
